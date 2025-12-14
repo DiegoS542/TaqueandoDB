307d21f371cc52839fc10121b5fdab0186175402
DiegoS542 <diegosalasp542@gmail.com>
2025-12-14 08:04:28 -0600

    Trigger para eliminar insumos

--- Backend/src/controllers/insumos.controller.js
+++ Backend/src/controllers/insumos.controller.js
@@ -103,36 +103,21 @@ const updateInsumo = async (req, res) => {
 // Eliminar Insumo
 const deleteInsumo = async (req, res) => {
     const id = parseInt(req.params.id);
-    const client = await db.pool.connect();
+
 
     try {
-        await client.query('BEGIN');
-
-        // Borrar relaciones primero
-        await client.query(`DELETE FROM insumo_proveedor WHERE insumo_id = $1`, [id]);
-
-        // Borrar insumo
-        const result = await client.query(`DELETE FROM insumos WHERE insumo_id = $1 RETURNING *`, [id]);
-
-        if (result.rows.length === 0) {
-            throw new Error('Insumo no encontrado');
-        }
-
-        await client.query('COMMIT');
-        res.status(200).json({ message: 'Insumo eliminado' });
-
-    } catch (error) {
-        await client.query('ROLLBACK');
-        console.error(error);
-        if (error.message === 'Insumo no encontrado') {
+        const query = 'DELETE FROM insumos WHERE insumo_id = $1 RETURNING *';
+        const response = await db.query(query, [id]);
+        
+        if (response.rows.length === 0) {
             return res.status(404).json({ error: 'Insumo no encontrado' });
         }
-        if (error.code === '23503') {
-            return res.status(400).json({ error: 'No se puede eliminar: Este insumo se usa en productos/recetas.' });
-        }
-        res.status(500).json({ error: 'Error al eliminar' });
-    } finally {
-        client.release();
+        
+        res.status(200).json({ message: 'Insumo eliminado correctamente' });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error interno al eliminar insumo' });
     }
 };
 
